938d5f42c850849cf5779e7857c9edd0f24ac111
tavamhieu <125560675+tavamhieu@users.noreply.github.com>
2023-03-26 15:39:28 +0700

    Add files via upload

---
+++ baithuchanhlap3/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        
+     hinhtron ht = new hinhtron();
+        ht.nhapbankinh();
+        ht.tinhchuvi();
+        ht.tinhdientich();
+
+
+
+}
+}
---
+++ baithuchanhlap3/src/hinhtron.java
@@ -0,0 +1,34 @@
+public class hinhtron{
+    final float pi = 3.14f ;
+    float r;
+    float chuvi=0;
+    float dientich = 0;
+    
+    hinhtron ht = new hinhtron();
+     void  nhapbankinh() {
+        System.out.printf(" nhap ban kinh %f",r);
+        
+    } void tinhchuvi (){
+        chuvi= chuvi+2*pi*r;
+       
+        
+    }
+     void tinhdientich()
+    {
+        dientich= dientich + 2*pi*(r*r);
+        
+    }
+    
+
+
+
+
+
+    }
+
+    
+
+
+
+ 
+  
